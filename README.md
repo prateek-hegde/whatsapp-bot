efd0fc48b83d8bd9fd6a7b907e72e1b0a6620694
Prateek Hegde <prateek@Prateeks-MacBook-Pro.local>
2021-12-31 15:46:54 +0530

    whatsapp bot for new year

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to Use?
+1. Install nodeJs 12+
+2. Run the scirpt, it opens web whatsapp app on the browser
+3. Scan QR code from your whatsapp.
+4. Then script will do the work for you.
---
+++ app.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const { Client } = require('whatsapp-web.js');
+const SESSION_FILE_PATH = './session.json';
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionCfg = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({ puppeteer: { headless: false }, session: sessionCfg });
+
+client.initialize();
+
+const msgList = [
+    'Happy New Year',
+    'new year',
+    'hny',
+    'this year',
+    'hpy new yr',
+    'hpy nw year',
+    '2022'
+];
+const replies = [
+    'Thank you :) wish you the same',
+    'Same to you :)'
+]
+
+
+
+client.on('qr', (qr) => {
+    // NOTE: This event will not be fired if a session is specified.
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('authenticated', (session) => {
+    console.log('AUTHENTICATED', session);
+    sessionCfg = session;
+    if (session) {
+        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
+
+});
+
+client.on('auth_failure', msg => {
+    // Fired if session restore was unsuccessfull
+    console.error('AUTHENTICATION FAILURE', msg);
+});
+
+client.on('ready', () => {
+    console.log('READY');
+});
+
+client.on('message', message => {
+    const isNewYearWish= msgList.find((msg) => message.body.toLocaleLowerCase().includes(msg.toLocaleLowerCase()));
+
+    if (isNewYearWish) {
+        const reply = replies[Math.floor(Math.random() * replies.length)];
+        message.reply(reply)
+    }
+
+});
+
+client.on('change_state', state => {
+    console.log('CHANGE STATE', state);
+});
+
+client.on('disconnected', (reason) => {
+    console.log('Client was logged out', reason);
+});
